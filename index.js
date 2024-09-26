a7cd930e78e10684bb5413b92fb94a8ccf8d8ca2
emmanuelcardona1 <163427104+emmanuelcardona1@users.noreply.github.com>
2024-09-26 15:31:51 -0500

    cambio login

---
+++ index.js
@@ -0,0 +1,22 @@
+function ingresar() {
+    let usuario = document.getElementById("usuario").value;
+    let contraseña = document.getElementById("contraseña").value;
+    let verificacionusuario1 = "admin1";
+    let verificacioncontraseña1 = "123456";
+    let verificacionusuario2 = "admin";
+    let verificacioncontraseña2 = "admin1234";
+  
+    // Verificación de usuario y contraseña
+    if (
+      (usuario === verificacionusuario1 &&
+        contraseña === verificacioncontraseña1) ||
+      (usuario === verificacionusuario2 && contraseña === verificacioncontraseña2)
+    ) {
+      // Redirige a otra página si las credenciales son correctas
+      window.location.href = "./pagina_principal.html";
+    } else {
+      // Muestra alerta si las credenciales son incorrectas
+      alert("Usuario o contraseña inválidos");
+    }
+  }
+  
